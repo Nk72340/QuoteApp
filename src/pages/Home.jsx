547f37f08f1c0e076d6907a0f17e513be3e51a0a
Narendra Kumar <83486558+MixupKnowledge@users.noreply.github.com>
2023-05-31 19:21:48 +0530

    Add files via upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "../components/Login";
+import Quotes from "../components/Quotes";
+const Home = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route path="/quotes" element={<Quotes />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
+export default Home;
